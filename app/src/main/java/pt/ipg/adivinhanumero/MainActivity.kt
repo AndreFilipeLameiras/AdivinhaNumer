00fcae2021e1091f9dcd0ae2be76aa971c6c8b20
1700017 <afplameiras@gmail.com>
2022-05-11 09:29:37 +0100

    Gerar numero aleatorio para o utilizador adivinhar

--- app/src/main/java/pt/ipg/adivinhanumero/MainActivity.kt
+++ app/src/main/java/pt/ipg/adivinhanumero/MainActivity.kt
@@ -2,10 +2,25 @@ package pt.ipg.adivinhanumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var numeroAdivinhar: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        numeroAdivinhar = Random.nextInt(NUMERO_ADIVINHAR_MINIMO, NUMERO_ADIVINHAR_MAXIMO + 1)
+    }
+
+
+    
+    companion object{
+        const val NUMERO_ADIVINHAR_MINIMO = 1
+        const val NUMERO_ADIVINHAR_MAXIMO = 10
     }
 }
